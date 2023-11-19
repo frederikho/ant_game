9d6526c04585d6d82874912521ec83d98753b4e8
frederikho <frederik.holtel@gmx.net>
2023-11-19 14:35:08 +0100

    Add start scene

---
+++ Assets/Scripts/SceneLoader2.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+public class SceneLoader2 : MonoBehaviour
+{
+    [Header("Buttons")]
+    public Button button1;
+    
+    // This method is called when the button is clicked
+    public void Start ()
+    {
+        button1.onClick.AddListener(LoadScene);
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("Ants");
+    }
+}
