c822a3d8d2b3785ddd685dd2d6d00121bd0ca756
frederikho <frederik.holtel@gmx.net>
2023-11-19 14:00:33 +0100

    Add art to end scene

--- Assets/Scenes/SceneLoader.cs
+++ Assets/Scenes/SceneLoader.cs
@@ -1,9 +1,22 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 public class SceneLoader : MonoBehaviour
 {
+    [Header("Buttons")]
+    public Button button1;
+    
     // This method is called when the button is clicked
+    public void Start ()
+    {
+        button1.onClick.AddListener(LoadScene);
+    }
+
     public void LoadScene()
     {
         SceneManager.LoadScene("The Ant");
